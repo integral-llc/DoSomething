f837a3c2f8577cfa61167a775bafe7e99793b21f
Eugene Rata <eugen.rata@gmail.com>
2019-06-03 08:11:12 -0700

    Updated assembly info and Readme file.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DoSomethingEx")]
+[assembly: AssemblyTitle("DoSomething")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DoSomethingEx")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Do Something")]
+[assembly: AssemblyCopyright("Integral Consulting LLC - ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # DoSomething
+An application that simply simulates mouse move & click. Made mostly to fool the Windows and Skype for Business to show that a person is online. :) 
+
+# Plans
+Might add some screen capturing in case some events happen. Any ideas are welcome. 
+
