e4069f95b755d225a56d7f186641d108f99493b6
Eugene Rata <eugen.rata@gmail.com>
2019-06-03 08:05:18 -0700

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DoSomethingEx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.tmrWorker = new System.Windows.Forms.Timer(this.components);
+            this.numStopAfter = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tmrStop = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numStopAfter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(12, 12);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Tag = "0";
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // tmrWorker
+            // 
+            this.tmrWorker.Interval = 10;
+            this.tmrWorker.Tick += new System.EventHandler(this.TmrWorker_Tick);
+            // 
+            // numStopAfter
+            // 
+            this.numStopAfter.Location = new System.Drawing.Point(98, 40);
+            this.numStopAfter.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numStopAfter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numStopAfter.Name = "numStopAfter";
+            this.numStopAfter.Size = new System.Drawing.Size(120, 20);
+            this.numStopAfter.TabIndex = 1;
+            this.numStopAfter.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Stop After (min)";
+            // 
+            // tmrStop
+            // 
+            this.tmrStop.Tick += new System.EventHandler(this.TmrStop_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 153);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numStopAfter);
+            this.Controls.Add(this.btnStartStop);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Do Something";
+            ((System.ComponentModel.ISupportInitialize)(this.numStopAfter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Timer tmrWorker;
+        private System.Windows.Forms.NumericUpDown numStopAfter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer tmrStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace DoSomethingEx
+{
+    public partial class Form1 : Form
+    {
+        private Point _start, _end;
+        private int _curIndex;
+        private List<Point> _points = new List<Point>();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public IEnumerable<Point> YieldLinePoints(int x, int y, int x2, int y2)
+        {
+            int w = x2 - x;
+            int h = y2 - y;
+            int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+            if (w < 0) dx1 = -1;
+            else if (w > 0) dx1 = 1;
+            if (h < 0) dy1 = -1;
+            else if (h > 0) dy1 = 1;
+            if (w < 0) dx2 = -1;
+            else if (w > 0) dx2 = 1;
+            int longest = Math.Abs(w);
+            int shortest = Math.Abs(h);
+            if (!(longest > shortest))
+            {
+                longest = Math.Abs(h);
+                shortest = Math.Abs(w);
+                if (h < 0) dy2 = -1;
+                else if (h > 0) dy2 = 1;
+                dx2 = 0;
+            }
+
+            int numerator = longest >> 1;
+            for (int i = 0; i <= longest; i++)
+            {
+                //putpixel(x, y, color);
+                yield return new Point(x, y);
+                numerator += shortest;
+                if (!(numerator < longest))
+                {
+                    numerator -= longest;
+                    x += dx1;
+                    y += dy1;
+                }
+                else
+                {
+                    x += dx2;
+                    y += dy2;
+                }
+            }
+        }
+
+        private double Distance(Point p1, Point p2)
+        {
+            var dy = p2.Y - p1.Y;
+            var dx = p2.X - p1.X;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        private Random R = new Random();
+        private Point GetPoint(Point p, double minDistance)
+        {
+            Rectangle rc = Screen.PrimaryScreen.WorkingArea;
+            int x, y;
+            while (true)
+            {
+                x = R.Next(rc.Left, rc.Right);
+                y = R.Next(rc.Top, rc.Bottom);
+                Point pNew = new Point(x, y);
+                bool ok = Distance(pNew, p) >= minDistance;
+                if (ok)
+                    return pNew;
+            }
+        }
+
+        private void TmrWorker_Tick(object sender, EventArgs e)
+        {
+            Point p = _points[_curIndex];
+            SetCursorPos(p.X, p.Y);
+            _curIndex++;
+            if (_curIndex >= _points.Count)
+            {
+                _points.Clear();
+                _start = _end;
+                _end = GetPoint(_start, 300);
+                _points.AddRange(YieldLinePoints(_start.X, _start.Y, _end.X, _end.Y));
+                _curIndex = 0;
+                mouse_event(MOUSEEVENTF_LEFTUP | MOUSEEVENTF_LEFTDOWN, (uint) _start.X, (uint) _start.Y, 0, 0);
+            }
+        }
+
+        private void TmrStop_Tick(object sender, EventArgs e)
+        {
+            btnStartStop.PerformClick();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int tag = Convert.ToInt32(btnStartStop.Tag);
+            if (tag == 0)
+            {
+                _start = GetPoint(new Point(0, 0), 100);
+                _end = GetPoint(_start, 300);
+                _points.Clear();
+                _points.AddRange(YieldLinePoints(_start.X, _start.Y, _end.X, _end.Y));
+                _curIndex = 0;
+                tmrWorker.Start();
+                WindowState = FormWindowState.Minimized;
+                var ts = TimeSpan.FromMinutes((double) numStopAfter.Value);
+                tmrStop.Interval = (int) ts.TotalMilliseconds;
+                tmrStop.Start();
+            }
+            else
+            {
+                tmrWorker.Stop();
+                tmrStop.Stop();
+                WindowState = FormWindowState.Normal;
+            }
+
+            tag = 1 - tag;
+            btnStartStop.Tag = tag;
+            btnStartStop.Text = tag == 0 ? "Start" : "Stop";
+        }
+    }
+}
